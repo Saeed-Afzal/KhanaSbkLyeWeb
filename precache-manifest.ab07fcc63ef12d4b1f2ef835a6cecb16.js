dff49202fba728a51bf02f3fbc723f9675d8ace0
Saeed-Afzal <saeedafzal429@gmail.com>
2022-01-03 16:37:00 +0500

    Updates

---
+++ precache-manifest.ab07fcc63ef12d4b1f2ef835a6cecb16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93477a4e5ca4176aa62ca15105a0e1c4",
+    "url": "/kahan-sb-k-lye/index.html"
+  },
+  {
+    "revision": "9a328807eb54bfb75159",
+    "url": "/kahan-sb-k-lye/static/css/2.0aed6780.chunk.css"
+  },
+  {
+    "revision": "9a328807eb54bfb75159",
+    "url": "/kahan-sb-k-lye/static/js/2.cec3f208.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/kahan-sb-k-lye/static/js/2.cec3f208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769717d1946d725187a8",
+    "url": "/kahan-sb-k-lye/static/js/main.af0dbf3e.chunk.js"
+  },
+  {
+    "revision": "28e214c4012e210c10d6",
+    "url": "/kahan-sb-k-lye/static/js/runtime-main.75ca84ea.js"
+  },
+  {
+    "revision": "70fcb2811b43c1868149cafd8ea881de",
+    "url": "/kahan-sb-k-lye/static/media/1.70fcb281.jpg"
+  },
+  {
+    "revision": "0cdf951e83287f31534112b0bd25d454",
+    "url": "/kahan-sb-k-lye/static/media/2.0cdf951e.jpg"
+  },
+  {
+    "revision": "fdc43e3358051b0593e9c4594456a07e",
+    "url": "/kahan-sb-k-lye/static/media/3.fdc43e33.jpg"
+  },
+  {
+    "revision": "c8ebd822dfdc312f5e6da35920fa6740",
+    "url": "/kahan-sb-k-lye/static/media/logo.c8ebd822.png"
+  }
+]);
